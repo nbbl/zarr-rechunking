e72d1deb4f89c3d758195edf4e2bb90a6e7e4e32
Johannes Sauer <jtsauer@gmail.com>
2023-05-06 19:30:32 +0200

    metadata writing and more data validity checks

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use clap::Parser;
 use json::JsonValue;
-use std::path::{Path, PathBuf};
-use std::{fs, io, usize};
+use std::fs::{self, OpenOptions};
+use std::io::{self, Write};
+use std::path::{Component, Path, PathBuf};
+use std::usize;
 use thiserror::Error;
 
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
-    root: String,
+    top: String,
 
     #[arg(short, long)]
     array: String,
@@ -16,9 +18,9 @@ struct Args {
     output: String,
 }
 
+// TODO: Use anyhow (sic!) crate instead?
 #[derive(Error, Debug)]
 enum RechunkingError {
-    // TODO: different error types (metadata, chunks, ...)?
     #[error("TODO")]
     IncompatibleZarrVersion,
 
@@ -30,6 +32,9 @@ enum RechunkingError {
 
     #[error("TODO")]
     InvalidJSON,
+
+    #[error("TODO")]
+    InvalidMetadataFile,
 
     #[error("TODO")]
     InvalidChunkFiles,
@@ -51,16 +56,19 @@ fn parse_zarray(in_dir: &Path) -> Result<Metadata, RechunkingError> {
     let zarray_file = in_dir.join(".zarray");
     let json = json::parse(&fs::read_to_string(zarray_file.as_path())?)
         .map_err(|_| RechunkingError::InvalidJSON)?;
-    if json["zarr_format"] != 2 {
+    if !json.has_key("zarr_format") || json["zarr_format"] != 2 {
         return Err(RechunkingError::IncompatibleZarrVersion);
     }
-    if json["chunks"].members().as_slice() != [1] {
+    if !json.has_key("chunks") || json["chunks"].members().as_slice() != [1] {
         return Err(RechunkingError::IncompatibleChunkSize);
     }
-    let [shapes] = json["shape"].members().as_slice() else {
+    if !json.has_key("shape") {
+        return Err(RechunkingError::IncompatibleArrayShape);
+    }
+    let [s] = json["shape"].members().as_slice() else {
         return Err(RechunkingError::IncompatibleArrayShape)
     };
-    let shape = shapes
+    let shape = s
         .as_usize()
         .ok_or(RechunkingError::IncompatibleArrayShape)?;
     Ok(Metadata { json, shape })
@@ -121,31 +129,100 @@ fn write_chunk(out_path: &Path, arr_buf: Vec<u8>) -> io::Result<()> {
     fs::write(out_path.join("0"), arr_buf)
 }
 
-fn adjust_metadata(in_data: JsonValue, chunk_size: usize) -> JsonValue {
+fn adjust_zarray(in_data: JsonValue, chunk_size: usize) -> JsonValue {
     let mut out = in_data;
     out["chunks"] = json::array![chunk_size];
+    // TODO: Adapt compression options
     out
 }
 
-fn write_zarray(out_path: &Path, data: JsonValue) -> io::Result<()> {
-    fs::write(out_path.join(".zarray"), json::stringify_pretty(data, 4))
+fn write_metadata(
+    top_dir: &Path,
+    rel_in_dir: &Path,
+    rel_out_dir: &Path,
+    out_data: JsonValue,
+) -> Result<(), RechunkingError> {
+    let out_zarray = rel_out_dir.join(".zarray");
+    let out_zattrs = rel_out_dir.join(".zattrs");
+    let in_zattrs = rel_in_dir.join(".zattrs");
+    let in_out_zmetadata = top_dir.join(".zmetadata");
+
+    if in_out_zmetadata.exists() {
+        let mut top_json = json::parse(&fs::read_to_string(&in_out_zmetadata)?)
+            .map_err(|_| RechunkingError::InvalidJSON)?;
+        if !top_json.has_key("metadata") {
+            return Err(RechunkingError::InvalidMetadataFile);
+        }
+
+        let metadata = &mut top_json["metadata"];
+        let out_zarray_str: &str = &out_zarray.to_string_lossy();
+        // NOTE: No check if metadata contains zarray key of input
+        metadata[out_zarray_str] = out_data.clone();
+
+        let in_zattrs_str: &str = &in_zattrs.to_string_lossy();
+        if metadata.has_key(in_zattrs_str) {
+            let out_zattrs_str: &str = &out_zattrs.to_string_lossy();
+            metadata[out_zattrs_str] = metadata[in_zattrs_str].clone();
+        }
+
+        // TODO: Why does fs::write not always overwrite as documented?
+        let mut f = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(in_out_zmetadata)?;
+        f.write_all(json::stringify_pretty(top_json, 4).as_bytes())?;
+    }
+
+    if top_dir.join(&in_zattrs).exists() {
+        fs::copy(top_dir.join(in_zattrs), top_dir.join(out_zattrs))?;
+    }
+
+    fs::write(
+        top_dir.join(out_zarray),
+        json::stringify_pretty(out_data, 4),
+    )?;
+    Ok(())
+}
+
+fn is_normal_comp(path: &Path) -> bool {
+    let comps: Vec<Component> = path.components().collect();
+    comps.len() == 1 && comps.iter().all(|c| matches!(c, Component::Normal { .. }))
 }
 
 fn main() -> Result<(), RechunkingError> {
     let args = Args::parse();
-    // TODO: Check whether args are valid
-    let in_dir = Path::new(&args.array);
-    let metadata = parse_zarray(in_dir)?;
-    println!("Array shape: {:?}", metadata.shape);
-    // TODO: Check that arg for out_dir is single component, not path.
-    let out_dir = in_dir
+    // TODO: Move args checks into separate fn?
+    let top_dir = Path::new(&args.top);
+    if !top_dir.is_dir() {
+        // TODO: error msg.
+        return Err(RechunkingError::InvalidArgError);
+    }
+    let rel_in_dir = Path::new(&args.array);
+    let in_dir = top_dir.join(rel_in_dir);
+    if !in_dir.is_dir() {
+        // TODO: error msg.
+        return Err(RechunkingError::InvalidArgError);
+    }
+    let out_comp = Path::new(&args.output);
+    if !is_normal_comp(out_comp) {
+        // TODO: error msg.
+        return Err(RechunkingError::InvalidArgError);
+    }
+    let rel_out_dir = rel_in_dir
         .parent()
         .ok_or(RechunkingError::InvalidArgError)?
-        .join(&args.output);
-    let chunks = collect_chunks(in_dir, metadata.shape)?;
+        .join(out_comp);
+    let out_dir = top_dir.join(rel_out_dir.clone());
+    // TODO: Handle case that out_dir exits or is equal to array_dir (forbid overwriting?!)
+    let metadata = parse_zarray(&in_dir)?;
+    let chunks = collect_chunks(&in_dir, metadata.shape)?;
     let num_chunks = chunks.len();
     write_chunk(&out_dir, concat_chunks(chunks))?;
-    // TODO: Copy .zattrs too.
-    write_zarray(&out_dir, adjust_metadata(metadata.json, num_chunks))?;
+    write_metadata(
+        top_dir,
+        rel_in_dir,
+        &rel_out_dir,
+        adjust_zarray(metadata.json, num_chunks),
+    )?;
     Ok(())
 }
